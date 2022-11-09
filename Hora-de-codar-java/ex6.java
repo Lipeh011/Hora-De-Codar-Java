faba154d9f0e419d8a46539a3149153898d9cf51
Filipe Damasceno <110691973+Lipeh011@users.noreply.github.com>
2022-11-09 16:24:46 -0300

    Update ex6.java

--- Hora-de-codar-java/ex6.java
+++ Hora-de-codar-java/ex6.java
@@ -1,42 +1,26 @@
-import java.util.Locale;
+
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+   
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Digite três numeros:");
+    
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
 
-	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-      
-		int a, b, c;
-		
-		a = sc.nextInt();
-		b = sc.nextInt();
-		c = sc.nextInt();
-		
-	    if (a == b) {
-	    	System.out.println("O numero é igual ");
-	    }
-	    
-	    	if (a > b) {
-	    	System.out.println("O numero maior é " + a);
-	    	}   
-	    
-	    else if (b == c){
-	    	System.out.println("O numero é igual");	
-	    }
-	    	
-	        if (b > c) {
-	    	System.out.println("O numero maior é "+ b);
-	    }
-	        
-	        else if (c == b) {
-	    	System.out.println("O numero é igual");
-	    }
-	        else {
-	        	System.out.println("O numero maior é "+ c);
-	        }
-	    
-	    
-	}
-
+        if (a > b && a > c) {
+            System.out.println("O numero maior é" + a );
+        }
+        else if (b > a && b > c) {
+            System.out.println("O numero maior é " + b);
+        }
+        else if (c > a && c > b) {
+            System.out.println("O numero maior é " + c);
+        }
+        sc.close();
+    }
 }
